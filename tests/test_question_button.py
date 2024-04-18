0870f9bf1a3350018139bb6800398aebc5ace3ac
Sasaraf1992 <zelen20@gmail.com>
2024-04-18 22:44:19 +0300

    Поправил тесты

--- tests/test_question_button.py
+++ tests/test_question_button.py
@@ -1,83 +1,92 @@
-from qa_python.Sprint_6 import settings
-from selenium import webdriver
-from qa_python.Sprint_6.pages.main_page import YandexScooterMainPage
+import allure
+from main_page import YandexScooterMainPage
+from base_page import YandexScooterBase
 
 
 class TestYandexScooterQuestionAbout:
-    driver = None
 
-    @classmethod
-    def setup_class(cls):
-        cls.driver = webdriver.Chrome()
-
-    def test_how_much_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Сколько это стоит?"')
+    def test_how_much_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
         main_page.scroll_script()
         main_page.click_how_much_button()
-        assert main_page.get_how_much_text() == "Сутки — 400 рублей. Оплата курьеру — наличными или картой."
+        assert main_page.check_how_much_text() == "Сутки — 400 рублей. Оплата курьеру — наличными или картой."
 
-    def test_few_scooter_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Хочу несколько самокатов"')
+    def test_few_scooter_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
         main_page.scroll_script()
         main_page.click_few_scooter_button()
-        assert main_page.get_few_scooter_text() == ("Пока что у нас так: один заказ — один самокат. Если хотите "
-                                                    "покататься с друзьями, можете просто сделать несколько заказов —"
-                                                    " один за другим.")
+        assert main_page.check_few_scooter_text() == ("Пока что у нас так: один заказ — один самокат. Если хотите "
+                                                      "покататься с друзьями, можете просто сделать несколько заказов —"
+                                                      " один за другим.")
 
-    def test_how_arenda_time_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Расчёт время аренды"')
+    def test_how_arenda_time_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
         main_page.scroll_script()
         main_page.click_how_arenda_time_button()
-        assert main_page.get_how_arenda_time_text() == (
+        assert main_page.check_how_arenda_time_text() == (
             'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в '
             'течение дня. Отсчёт времени аренды начинается с момента, '
             'когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в '
             '20:30, суточная аренда закончится 9 мая в 20:30.')
 
-    def test_can_order_scooter_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Заказ самоката сегодня"')
+    def test_can_order_scooter_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
         main_page.scroll_script()
         main_page.click_can_order_scooter_button()
-        assert main_page.get_can_order_scooter_text() == ("Только начиная с завтрашнего дня. Но скоро станем "
-                                                          "расторопнее.")
+        assert main_page.check_can_order_scooter_text() == ("Только начиная с завтрашнего дня. Но скоро станем "
+                                                            "расторопнее.")
 
-    def test_can_return_scooter_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Вернуть самокат раньше"')
+    def test_can_return_scooter_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
         main_page.scroll_script()
         main_page.click_can_return_scooter_button()
-        assert main_page.get_can_return_scooter_text() == ("Пока что нет! Но если что-то срочное — всегда можно "
-                                                           "позвонить в поддержку по красивому номеру 1010.")
+        assert main_page.check_can_return_scooter_text() == ("Пока что нет! Но если что-то срочное — всегда можно "
+                                                             "позвонить в поддержку по красивому номеру 1010.")
 
-    def test_charger_with_scooter_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Зарядка с самокатом"')
+    def test_charger_with_scooter_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
         main_page.scroll_script()
         main_page.click_charger_with_scooter_button()
-        assert main_page.get_charger_with_scooter_text() == ("Самокат приезжает к вам с полной зарядкой. Этого хватает "
-                                                             "на восемь суток — даже если будете кататься без "
-                                                             "передышек и во сне. Зарядка не понадобится.")
+        assert main_page.check_charger_with_scooter_text() == (
+            "Самокат приезжает к вам с полной зарядкой. Этого хватает "
+            "на восемь суток — даже если будете кататься без "
+            "передышек и во сне. Зарядка не понадобится.")
 
-    def test_can_cancel_order_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Отмена заказа"')
+    def test_can_cancel_order_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
         main_page.scroll_script()
         main_page.click_can_cancel_order_button()
-        assert main_page.get_can_cancel_order_text() == ("Да, пока самокат не привезли. Штрафа не будет, "
-                                                         "объяснительной записки тоже не попросим. Все же свои.")
+        assert main_page.check_can_cancel_order_text() == ("Да, пока самокат не привезли. Штрафа не будет, "
+                                                           "объяснительной записки тоже не попросим. Все же свои.")
 
-    def test_leave_za_mkadom_button_active(self):
-        self.driver.get(settings.URL)
-        main_page = YandexScooterMainPage(self.driver)
+    @allure.title('Проверка открытия текста под кнопкой "Жизнь за МКАДом"')
+    def test_leave_za_mkadom_button_active(self, driver):
+        main_page = YandexScooterMainPage(driver)
+        bp = YandexScooterBase(driver)
+        bp.click_cookie_button()
+        main_page.wait_until_questions_download()
         main_page.scroll_script()
         main_page.click_leave_za_mkadom_button()
-        assert main_page.get_leave_za_mkadom_text() == ("Да, обязательно. Всем самокатов! И Москве, и Московской "
-                                                        "области.")
-
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
+        assert main_page.check_leave_za_mkadom_text() == ("Да, обязательно. Всем самокатов! И Москве, и Московской "
+                                                          "области.")
--- tests/test_scooter_order.py
+++ tests/test_scooter_order.py
@@ -1,47 +1,34 @@
-from qa_python.Sprint_6 import settings
-from selenium import webdriver
-from qa_python.Sprint_6.pages.order_page import YandexScooterOrder
-from qa_python.Sprint_6.pages.base_page import YandexScooterBase
+import allure
+from order_page import YandexScooterOrder
+from base_page import YandexScooterBase
 import pytest
-from qa_python.Sprint_6.data import ScooterTestData as SC
+from data import ScooterTestData as SC
 
 
 class TestYandexScooterOrderPage:
-    driver = None
-
-    @classmethod
-    def setup_class(cls):
-        cls.driver = webdriver.Firefox()
-
-
-    @pytest.mark.parametrize("name,surname,address,phone,click_cookie", [SC.USER_1,SC.USER_2])
-    def test_button_order_on_header(self, name, surname, address, phone, click_cookie):
-        self.driver.get(settings.URL)
-        bp = YandexScooterBase(self.driver)
+    @allure.title('Проверка успешного заказа самоката с двумя наборами данных"')
+    @pytest.mark.parametrize("name,surname,address,phone", [SC.USER_1, SC.USER_2])
+    def test_button_order_on_header(self,driver, name, surname, address, phone):
+        bp = YandexScooterBase(driver)
         bp.click_order_button_header()
-        op = YandexScooterOrder(self.driver)
+        op = YandexScooterOrder(driver)
         op.set_name(name)
         op.set_surname(surname)
         op.set_address(address)
         op.click_metro()
         op.set_metro_sokolniky()
         op.set_phone_number(phone)
-        if click_cookie:
-            op.click_cookie_button()
+        bp.click_cookie_button()
         op.click_next_button()
         op.click_date_field()
         op.set_date_twenty_five()
         op.click_rental_period()
         op.set_rental_date_three_days()
         op.set_scooter_color_gray()
-        self.driver.implicitly_wait(3)
         op.click_order_button()
-        self.driver.implicitly_wait(3)
         op.click_confirmation_уes_button()
         assert op.check_successful_order_header()
 
-        @classmethod
-        def teardown_class(cls):
-            cls.driver.quit()
+
 
 
